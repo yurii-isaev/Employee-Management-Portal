73c69ad84c885e79c3bf744d8e925d96a0c26181
Yurii-Isaev <devhouse27@gmail.com>
2021-05-03 20:47:13 +0200

    test: test method get department list in shared service

--- WebClient/src/app/shared.service.spec.ts
+++ WebClient/src/app/shared.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
 import { SharedService } from './shared.service';
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('SharedService', () => {
   let service: SharedService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule]
+    });
     service = TestBed.inject(SharedService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should be created', () => expect(service).toBeTruthy());
+
+  it('should returns list of departments', inject([SharedService, HttpTestingController],
+    (service: SharedService, backend: HttpTestingController) => {
+      const mockDepartments = [{test: 'test'}];
+      service.getDepList().subscribe(departments => expect(departments).toEqual(mockDepartments));
+      backend.expectOne({method: 'GET', url: 'http://localhost:5000/api/Department'}).flush(mockDepartments);
+    }));
 });
