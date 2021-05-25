e7d650d0a47eede7c64dd0a325264d2aa75cbdef
Yurii-Isaev <devhouse27@gmail.com>
2021-05-25 18:39:52 +0200

    refactor: shared service department methods

--- WebClient/src/app/shared.service.ts
+++ WebClient/src/app/shared.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IEmployee } from './employee/show-emp/show-emp';
+import { IDepartment } from './department/show-dep/show-dep';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SharedService {
-
   readonly APIUrl = 'http://localhost:5000/api';
   readonly PhotoUrl = 'http://localhost:5000/Photos/';
 
   constructor(private http: HttpClient) {}
 
-  getDepList(): Observable<any[]> {
-    return this.http.get<any>(this.APIUrl + '/Department');
+  getAllDepartmentsFromDB(): Observable<Array<string>> {
+    return this.http.get<Array<string>>(this.APIUrl + '/Department');
   }
 
-  addDepartment(val: any): Observable<any> {
-    return this.http.post(this.APIUrl + '/Department', val);
+  addDepartmentToDB(object: IDepartment): Observable<string> {
+    return this.http.post<string>(this.APIUrl + '/Department', object);
   }
 
-  updateDepartment(val: any): Observable<any> {
-    return this.http.put(this.APIUrl + '/Department', val);
+  updateDepartmentToDB(object: IDepartment): Observable<string> {
+    return this.http.put<string>(this.APIUrl + '/Department', object);
   }
 
-  deleteDepartment(val: any): Observable<any> {
-    return this.http.delete(this.APIUrl + '/Department/' + val);
+  deleteDepartmentFromDB(id: number): Observable<string> {
+    return this.http.delete<string>(this.APIUrl + '/Department/' + id);
   }
 
   getAllEmployeesFromDB(): Observable<Array<string>> {
