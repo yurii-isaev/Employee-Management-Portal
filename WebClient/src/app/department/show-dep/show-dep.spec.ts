470e9ff92a050b13a9e54fd6fc14739a23a2262d
Yurii-Isaev <devhouse27@gmail.com>
2021-05-26 17:24:15 +0200

    test: test method update department list

--- WebClient/src/app/department/show-dep/show-dep.spec.ts
+++ WebClient/src/app/department/show-dep/show-dep.spec.ts
@@ -1,42 +1,45 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { ShowDep } from './show-dep';
 import { SharedService } from '../../shared.service';
+import { FormsModule } from "@angular/forms";
 import { of } from 'rxjs';
 
 describe('ShowDepComponent', () => {
   let component: ShowDep;
   let fixture: ComponentFixture<ShowDep>;
   let service: SharedService;
-  let spy: jasmine.Spy;
   let mockList: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ShowDep ],
-      imports: [ HttpClientModule ]
+      imports: [ HttpClientModule, FormsModule ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ShowDep);
     component = fixture.componentInstance;
     service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
-    mockList = [1, 'test'];
-    spy = spyOn(service, 'getDepList').and.returnValue(of(mockList));
+    mockList = ['test'];
     fixture.detectChanges();
   });
 
-  it('should create', () => expect(component).toBeTruthy());
+  it('should create component', () =>
+    expect(component).toBeTruthy());
 
-  it('should call shared service', () => {
-    component.refreshDepList();
+  it('should call shared service when update department list', () => {
+    const spy = spyOn(service, 'getAllDepartmentsFromDB').and.returnValue(of(mockList));
+    component.updateDepList();
     expect(spy.calls.any()).toBeTruthy();
   });
 
-  it('should set value department list', () => {
-    component.refreshDepList();
-    expect(component.DepartmentList).toEqual(mockList);
+  it('should set value department list when update department list', () => {
+    spyOn(service, 'getAllDepartmentsFromDB').and.returnValue(of(mockList));
+    component.updateDepList();
+    expect(component.departmentList).toEqual(mockList);
+    expect(component.departmentListWithoutFilter).toEqual(mockList);
   });
 });
