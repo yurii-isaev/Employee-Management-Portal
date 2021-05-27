3fad32cbece6ed1a01330bb724227606fb702a8d
Yurii-Isaev <devhouse27@gmail.com>
2021-05-27 19:18:27 +0200

    test: fix test method get all departments in shared service

--- WebClient/src/app/shared.service.spec.ts
+++ WebClient/src/app/shared.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { SharedService } from './shared.service';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('SharedService', () => {
@@ -9,18 +9,19 @@ describe('SharedService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule, HttpClientTestingModule]
+      imports: [ HttpClientModule, HttpClientTestingModule ]
     });
     service = TestBed.inject(SharedService);
   });
 
-  it('should be created', () => expect(service).toBeTruthy());
+  it('should be created service', () =>
+    expect(service).toBeTruthy());
 
   it('should returns list of departments', inject([SharedService, HttpTestingController],
     (service: SharedService, backend: HttpTestingController) => {
-      const mockDepartments = [{test: 'test'}];
-      service.getDepList().subscribe(departments => expect(departments).toEqual(mockDepartments));
-      backend.expectOne({method: 'GET', url: 'http://localhost:5000/api/Department'}).flush(mockDepartments);
+      const mock = ['test'];
+      service.getAllDepartmentsFromDB().subscribe(deps => expect(deps).toEqual(mock));
+      backend.expectOne({method: 'GET', url: 'http://localhost:5000/api/Department'}).flush(mock);
     }));
 
   it('should returns update photo name', inject([SharedService, HttpTestingController],
